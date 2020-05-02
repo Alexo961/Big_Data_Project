d742ae58fe132640ce1f7a73f53225cb7d49ae97
Alexo961 <salessio96@hotmail.it>
2020-05-02 17:14:00 +0200

    secondo commit

---
+++ Progetto/src/main/java/Mapper/Job1Mapper.java
@@ -0,0 +1,20 @@
+package Mapper;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class Job1Mapper extends Mapper<LongWritable, Text, Text, Text> {
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+	
+	String line = value.toString();
+	String ticker = line.split(",")[0];
+	
+	}
+	
+}
