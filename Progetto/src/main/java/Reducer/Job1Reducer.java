51a6bdf947e93ebf0d40bc6a9dbdd282dcc20d54
Alexo961 <salessio96@hotmail.it>
2020-05-02 18:11:52 +0200

    quarto  commit oggetto supporto reducer

---
+++ Progetto/src/main/java/Reducer/Job1Reducer.java
@@ -0,0 +1,14 @@
+package Reducer;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Job1Reducer extends
+Reducer<Text, List<String>, Text, IntWritable> {
+
+}
---
+++ Progetto/src/main/java/Supports/OutputObject.java
@@ -0,0 +1,61 @@
+package Supports;
+
+import java.util.Date;
+
+public class OutputObject {
+
+	private String ogettoString1 ;
+	private Double ogettoDouble2 ;
+	private Double ogettoDouble3 ;
+	private Double ogettoDouble4 ;
+	private Double ogettoDouble5 ;
+	private Date ogettoDate;
+	public OutputObject(String ogettoString1, Double ogettoDouble2, Double ogettoDouble3, Double ogettoDouble4,
+			Double ogettoDouble5, Date ogettoDate) {
+		super();
+		this.ogettoString1 = ogettoString1;
+		this.ogettoDouble2 = ogettoDouble2;
+		this.ogettoDouble3 = ogettoDouble3;
+		this.ogettoDouble4 = ogettoDouble4;
+		this.ogettoDouble5 = ogettoDouble5;
+		this.ogettoDate = ogettoDate;
+	}
+	public String getOgettoString1() {
+		return ogettoString1;
+	}
+	public void setOgettoString1(String ogettoString1) {
+		this.ogettoString1 = ogettoString1;
+	}
+	public Double getOgettoDouble2() {
+		return ogettoDouble2;
+	}
+	public void setOgettoDouble2(Double ogettoDouble2) {
+		this.ogettoDouble2 = ogettoDouble2;
+	}
+	public Double getOgettoDouble3() {
+		return ogettoDouble3;
+	}
+	public void setOgettoDouble3(Double ogettoDouble3) {
+		this.ogettoDouble3 = ogettoDouble3;
+	}
+	public Double getOgettoDouble4() {
+		return ogettoDouble4;
+	}
+	public void setOgettoDouble4(Double ogettoDouble4) {
+		this.ogettoDouble4 = ogettoDouble4;
+	}
+	public Double getOgettoDouble5() {
+		return ogettoDouble5;
+	}
+	public void setOgettoDouble5(Double ogettoDouble5) {
+		this.ogettoDouble5 = ogettoDouble5;
+	}
+	public Date getOgettoDate() {
+		return ogettoDate;
+	}
+	public void setOgettoDate(Date ogettoDate) {
+		this.ogettoDate = ogettoDate;
+	}
+	
+	
+}
