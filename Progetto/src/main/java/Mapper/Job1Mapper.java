7a651891b9bd3c8ab7f8d211266dd5f834b9a9c6
euregab <52067670+euregab@users.noreply.github.com>
2020-05-02 21:07:11 +0200

    modified Supports classes: deleted OutputObject, then created StockObject modified Mapper, Reducer, SupportObject to support StockObject

--- Progetto/src/main/java/Mapper/Job1Mapper.java
+++ Progetto/src/main/java/Mapper/Job1Mapper.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import Supports.SupportObject;
+import Supports.StockObject;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
-public class Job1Mapper extends Mapper<LongWritable, Text, Text, List<String> > {
+public class Job1Mapper extends Mapper<LongWritable, Text, Text, StockObject> {
 	public void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
 	
@@ -18,8 +21,11 @@ public class Job1Mapper extends Mapper<LongWritable, Text, Text, List<String> > 
 	String ticker = line.split(",")[0];
     Text word = new Text(ticker);
     List<String> list = Arrays.asList(line.split(","));
+    StockObject so;
     
-    context.write(word, list);
+    so = SupportObject.transform(list);
+    
+    context.write(word, so);
  
  
  
--- Progetto/src/main/java/Supports/SupportObject.java
+++ Progetto/src/main/java/Supports/SupportObject.java
@@ -1,13 +1,18 @@
 package Supports;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
-class SupportObject {
+public class SupportObject {
 	
 	
 	private Double ogettoDouble ;
 	private LocalDate ogettoDate;
+	
+	private static String pattern = "yyyy/MM/dd";
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
 	
 	public SupportObject(Double ogettoDouble, LocalDate ogettoDate) {
 		super();
@@ -29,6 +34,30 @@ class SupportObject {
 
 	public void setOgettoDate(LocalDate ogettoDate) {
 		this.ogettoDate = ogettoDate;
+	}
+
+	public static StockObject transform(List<String> list) {
+		StockObject stock;
+		String ticker;
+		Double open;
+		Double close;
+		Double adj;
+		Double low;
+		Double high;
+		Long volume;
+		LocalDate date;
+		
+		try {ticker = list.get(0);} catch (Exception exc) {ticker = null;}
+		try {open = Double.parseDouble(list.get(1));} catch (Exception exc) {open = null;}
+		try {close = Double.parseDouble(list.get(2));} catch (Exception exc) {close = null;}
+		try {adj = Double.parseDouble(list.get(3));} catch (Exception exc) {adj = null;}
+		try {low = Double.parseDouble(list.get(4));} catch (Exception exc) {low = null;}
+		try {high = Double.parseDouble(list.get(5));} catch (Exception exc) {high = null;}
+		try {volume = Long.parseLong(list.get(6));} catch (Exception exc) {volume = null;}
+		try {date = LocalDate.parse(list.get(7), formatter);} catch (Exception exc) {date = null;}
+		
+		stock = new StockObject(ticker, open, close, adj, low, high, volume, date);
+		return stock;
 	}
 
 }
