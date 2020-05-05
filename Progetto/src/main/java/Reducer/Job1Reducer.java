9bac821d1467420eb8f094a19624d621348f195e
euregab <52067670+euregab@users.noreply.github.com>
2020-05-05 12:13:06 +0200

    resolved merge conflict

--- Progetto/src/main/java/Reducer/Job1Reducer.java
+++ Progetto/src/main/java/Reducer/Job1Reducer.java
@@ -2,6 +2,7 @@ package Reducer;
 
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -9,33 +10,12 @@ import org.apache.hadoop.mapreduce.Reducer;
 import Supports.StockObject;
 import Supports.SupportObject;
 
-<<<<<<< HEAD
 public class Job1Reducer extends
 Reducer<Text, StockObject, Text, List<String> > {
 	
 	public void reduce(Text key, Iterable<StockObject> values,
 			Context context) throws IOException, InterruptedException {
    
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-}
-=======
-public class Job1Reducer
-	extends	Reducer<Text, StockObject, Text, StockObject > {
-	
-	@Override
-	public void reduce(Text key, Iterable<StockObject> values,
-			Context context) throws IOException, InterruptedException {
-		
-	}
->>>>>>> origin/gabriele
+		}
+
  }
