ae0245962040961c7d5eb05638585938058663b0
Alexo961 <salessio96@hotmail.it>
2020-05-05 17:16:24 +0200

    cambiata ancora classe reduce3

--- Progetto/src/main/java/Reducer/Job1Reducer.java
+++ Progetto/src/main/java/Reducer/Job1Reducer.java
@@ -19,23 +19,26 @@ import Supports.SupportObject;
 public class Job1Reducer extends
 Reducer<Text, StockObject, Text, Text > {
 
-	
+
 	private Map<Text, OutputObject> reduceMap = new HashMap<Text, OutputObject>();
-	 private Map<Text, Double> daSortareMap = new HashMap<Text, Double>();
+	private Map<Text, Double> daSortareMap = new HashMap<Text, Double>();
+	Double variation;
 	
+	
+	Double valore_min;
+	Double valore_max;
+	StringBuilder sb;
+	int numero_chiavi;
+
 	public void reduce(Text key, Iterable<StockObject> values,
 			Context context) throws IOException, InterruptedException {
 
-		Double variation;
+		
 		int sum = 0;
 		int k= 0;
 		Double[]a = new Double[2];
 		StockObject[] first_last = new StockObject[2];
-		Double valore_min;
-		Double valore_max;
-		StringBuilder sb;
 
-		
 
 		for(StockObject value : values){
 			first_last = SupportObject.first_last(first_last, value);
@@ -47,23 +50,24 @@ Reducer<Text, StockObject, Text, Text > {
 		Double volume_medio = SupportObject.vol_med(sum, k);
 
 		variation = SupportObject.variationquot(first_last[0].getOpen(), first_last[1].getClose());
+		//inseriamo in mappa da sortare text e variation
 		valore_min = a[0];
 		valore_max = a[1];
 
+         //inseriamo in mappa reduce map text e output object
+		numero_chiavi= daSortareMap.keySet().size();
 
-		int numero_chiavi= daSortareMap.keySet().size();
 
-
-		@Override
-		protected void cleanup(Context context) throws IOException, InterruptedException {
-			Map<Text, Double> sortedMap = sortByValues(daSortareMap);
-			int counter = 0;
-			for (Text key : sortedMap.keySet()) {
-				if (counter++ == numero_chiavi) {
-					break;
-				}
-				context.write(key, sortedMap.get(key));
+	}
+	@Override
+	protected void cleanup(Context context) throws IOException, InterruptedException {
+		Map<Text, Double> sortedMap = sortByValues(daSortareMap);
+		int counter = 0;
+		for (Text key : sortedMap.keySet()) {
+			if (counter++ == numero_chiavi) {
+				break;
 			}
+			context.write(key, reduceMap.get(key));
 		}
 
 
