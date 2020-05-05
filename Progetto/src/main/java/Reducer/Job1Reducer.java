1b020f712641c039189dadcd2964988a54c28b7d
euregab <52067670+euregab@users.noreply.github.com>
2020-05-05 12:00:22 +0200

    added support object methods

--- Progetto/src/main/java/Reducer/Job1Reducer.java
+++ Progetto/src/main/java/Reducer/Job1Reducer.java
@@ -2,20 +2,19 @@ package Reducer;
 
 
 import java.io.IOException;
-import java.util.List;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 
 import Supports.StockObject;
+import Supports.SupportObject;
 
-public class Job1Reducer extends
-Reducer<Text, List<String>, Text, StockObject > {
+public class Job1Reducer
+	extends	Reducer<Text, StockObject, Text, StockObject > {
 	
-	public void reduce(Text key, Iterable<List<String>> values,
+	@Override
+	public void reduce(Text key, Iterable<StockObject> values,
 			Context context) throws IOException, InterruptedException {
-   
-}
+		
+	}
  }
