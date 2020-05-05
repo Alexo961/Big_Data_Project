ed48dbc07f43193c1f66b02a2d47aa36542f6867
euregab <52067670+euregab@users.noreply.github.com>
2020-05-05 16:26:56 +0200

    almost completed reducer

--- Progetto/src/main/java/Reducer/Job1Reducer.java
+++ Progetto/src/main/java/Reducer/Job1Reducer.java
@@ -16,28 +16,29 @@ public class Job1Reducer extends
 Reducer<Text, StockObject, Text, Text > {
 
 	public void reduce(Text key, Iterable<StockObject> values,
-			Context context) throws IOException, InterruptedException {
+		Context context) throws IOException, InterruptedException {
 
-
-		
+		Double variation;
 		int sum = 0;
 		int k= 0;
 		Double[]a = new Double[2];
-		
-		for(StockObject value : values) {
+		StockObject[] first_last = new StockObject[2];
+		Double min;
+		Double max;
+		StringBuilder sb;
+
+		for(StockObject value : values){
+			first_last = SupportObject.first_last(first_last, value);
 			sum += value.getVolume();
 			k +=1;
-			 a = SupportObject.min_max(a, value.getClose());
+			a = SupportObject.min_max(a, value.getClose());
 		}
-		Double volume_medio = SupportObject.vol_med(sum, k);
-
-
-
 		
+		Double volume_medio = SupportObject.vol_med(sum, k);
+		variation = SupportObject.variationquot(first_last[0].getOpen(), first_last[1].getClose());
+		min = a[0];
+		max = a[1];
 
-
-
+	}
 
 }
-
- }
