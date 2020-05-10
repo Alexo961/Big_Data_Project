d821b9a1763932c0c8924a7c0cf14c91e39cac10
Alexo961 <salessio96@hotmail.it>
2020-05-10 15:06:48 +0200

    inizojoin

--- MapReduce/src/main/java/mapper/FirstJobSecondMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class FirstJobSecondMapper extends Mapper<LongWritable, Text, Text, Text> {
-
-}
