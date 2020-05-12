b7747fe61096555a9b70540765eed57049f6a51a
euregab <52067670+euregab@users.noreply.github.com>
2020-05-12 19:38:27 +0200

    year

--- MapReduce/src/main/java/mapper/Job1_2.java
+++ MapReduce/src/main/java/mapper/Job1_2.java
@@ -1,6 +1,8 @@
 package mapper;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,27 +15,32 @@ import objects.StockObject;
 public class Job1_2 extends Mapper<LongWritable, Text, Text, Text> {
 
 	private Text ticker;
+	private static final String DATE_PATTERN = "yyyy-MM-dd";
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
 
 	@Override
 	public void map(LongWritable key, Text value, Context context) 
 			throws IOException, InterruptedException {
 
-		    
-			String line = value.toString();
-			String[] str = line.split(",");
-			if(str.length == 9) {
-			String settore_data = str[1]+str[8];
-			 Text chiave = new Text(settore_data);
-			 context.write(chiave, value);
-			}
-			
-			else {
-				
-				System.out.println("Riga spuria");
-			}
+		LocalDate date;
+
+		String line = value.toString();
+		String[] str = line.split(",");
+		if(str.length == 9) {
+			date = LocalDate.parse(str[8], DATE_TIME_FORMATTER);
+			Integer year = date.getYear();
+			String yearString = year.toString();
+			String settore_data = str[1]+ "_" + yearString;
+			Text chiave = new Text(settore_data);
+			context.write(chiave, value);
 		}
 
-		
+		else {
 
+			System.out.println("Riga spuria");
+		}
 	}
+
+
+
 }
